74f9464340b0480a8771034c4d9bd3d8c766b0a7
Jehyson <jehysonrizo@gmail.com>
2022-12-11 17:24:58 -0600

    creacion del login

--- src/main.js
+++ src/main.js
@@ -2,5 +2,5 @@ import { createApp } from 'vue';
 
 import App from './App.vue';
 import '@/assets/bootstrap.min.css'
-import '@/assets/bootstrap.bundle.min'
+import '@/assets/bootstrap.bundle.min.js'
 createApp(App).mount('#app');
---
+++ src/views/index.js
@@ -0,0 +1,1 @@
+export {default as Home_login} from './Home_login.vue';
